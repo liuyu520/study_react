9e26e0d164ece63de7763b821af73eb72e776d96
huangweii <huangweii@chanjet.com>
2016-09-30 15:40:14 +0800

    初始增加文件

---
+++ src/helloworld.js
@@ -0,0 +1,7 @@
+/**
+ * Created by 黄威 on 9/30/16.
+ */
+ReactDOM.render(
+<h1>Hello, world!</h1>,
+    document.getElementById('app')
+);
---
+++ src/main.js
@@ -0,0 +1,10 @@
+/**
+ * Created by 黄威 on 9/30/16.
+ */
+import React from 'react';
+import ReactDom from 'react-dom';
+import App from './src/app/App';
+ReactDom.render(
+    <App  rootPage='/home'/> ,
+    document.getElementById('app')
+);
